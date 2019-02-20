4bb41b4b3f581060099946e0695f02f8f1cc25a4
KLRussell <47677315+KLRussell@users.noreply.github.com>
2019-02-19 23:22:40 -0500

    Update Vacuum_Main.py

--- Vacuum_Main.py
+++ Vacuum_Main.py
@@ -8,6 +8,8 @@ import os
 
 SourceDir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
+SourceCodeDir = SourceDir + "\\03_Source_Code"
+
 UpdatesDir = []
 UpdatesDir.append(SourceDir + "\\01_Updates\\01_BMI-PCI")
 UpdatesDir.append(SourceDir + "\\01_Updates\\02_Seeds")
@@ -15,13 +17,13 @@ UpdatesDir.append(SourceDir + "\\01_Updates\\03_Non-Seeds")
 UpdatesDir.append(SourceDir + "\\01_Updates\\04_Dispute-Actions")
 UpdatesDir.append(SourceDir + "\\01_Updates\\05_New-User")
 
+Settings = dict()
+
 class XMLParseClass:
-    def __init__(self, folder_name, File, upload_date):
+    def __init__(self, File):
         try:
             tree = ET.parse(File)
-            self.folder_name = folder_name
             self.root = tree.getroot()
-            self.upload_date = upload_date
         except AssertionError as a:
             print('\t[-] {} : Parse failed.'.format(a))
             pass
@@ -30,20 +32,57 @@ class XMLParseClass:
         if parsed is None:
             parsed = dict()
 
-        for key in element.keys():
-            if key not in parsed:
-                parsed[key] = element.attrib.get(key)
+        if element.keys():
+            for key in element.keys():
+                if key not in parsed:
+                    parsed[key] = element.attrib.get(key)
 
-            if element.text and element.tag not in parsed:
-                parsed[element.tag] = element.text
+                if element.text and element.tag not in parsed:
+                    parsed[element.tag] = element.text
+
+        elif element.text and element.tag not in parsed:
+            parsed[element.tag] = element.text
+
 
         for child in list(element):
             self.ParseElement(child, parsed)
         return parsed
 
-    def ParseXML(self):
-        parsed = [self.ParseElement(item) for item in self.root.findall('./{urn:schemas-microsoft-com:rowset}data/')]
-        return pd.DataFrame(parsed)
+    def ParseXML(self, findpath, dict=False):
+        if dict:
+            parsed = None
+
+            for item in self.root.findall(findpath):
+                parsed = self.ParseElement(item, parsed)
+
+            return parsed
+        else:
+            parsed = [self.ParseElement(item) for item in self.root.findall(findpath)]
+            return pd.DataFrame(parsed)
+
+def Get_Global(Setting_Name):
+    for a, b in Settings:
+        print(a)
+
+
+def Load_Settings():
+    Settings = dict()
+    XMLObj = XMLParseClass(SourceCodeDir + "\\Vacuum_Settings.xml")
+
+    if XMLObj:
+        Settings = XMLObj.ParseXML('./Settings/Network/', True)
+        Settings = XMLObj.ParseXML('./Settings/Read_Write_TBL/', True)
+        Settings = XMLObj.ParseXML('./Settings/Read_TBL/', True)
+
+        DF = XMLObj.ParseXML('./Settings/CAT_Workbook/BMIPCI_Review/Action/')
+        Settings = {'BMIPCI-Action': DF.loc[:, 'Action'].values}
+
+        DF = XMLObj.ParseXML('./Settings/CAT_Workbook/Dispute_Actions/Action/')
+        Settings = {'Dispute_Actions-Action': DF.loc[:, 'Action'].values}
+
+        return Settings
+    else:
+        raise ValueError("Unable to load Vacuum_Settings.xml. Please check path and file {0}".format(SourceCodeDir))
 
 def Check_For_Updates():
     for DirPath in UpdatesDir:
@@ -52,19 +91,20 @@ def Check_For_Updates():
             return Files
 
 def Process_Updates(Files):
-    XMLObj = None
-
     for File in Files:
         upload_date = datetime.now()
         folder_name = os.path.basename(os.path.dirname(os.path.dirname(File)))
-        XMLObj = XMLParseClass(folder_name, File, upload_date)
+        XMLObj = XMLParseClass(File)
 
         if XMLObj:
-            print(XMLObj.ParseXML())
+            Parsed = XMLObj.ParseXML('./{urn:schemas-microsoft-com:rowset}data/')
+            #print(Parsed.loc[Parsed['Action'] == 'Open Inquiry'])
+
 
 if __name__ == '__main__':
     Has_Updates = None
-
+    Settings = Load_Settings()
+    print(Get_Global('Dispute_Actions-Action'))
     while (Has_Updates is None):
         Has_Updates = Check_For_Updates()
         sleep(1)
