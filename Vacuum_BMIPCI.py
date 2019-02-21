d42cf5bba3c06a6b4501ee0c0385e1e0bdef0baf
KLRussell <47677315+KLRussell@users.noreply.github.com>
2019-02-21 17:42:26 -0500

    Update Vacuum_BMIPCI.py

--- Vacuum_BMIPCI.py
+++ Vacuum_BMIPCI.py
@@ -1,68 +1,118 @@
 from Vacuum_Global import Settings
 from Vacuum_Global import SQLConnect
+from Vacuum_Global import Append_Errors
 
 import pandas as pd
 
 #self.DF.loc[self.DF['Gs_SrvType'] == 'LL',['Source_TBL','Source_ID','Gs_SrvID']]
 
 class BMIPCI:
-    Errors = pd.DataFrame()
+    Success = False
 
     def __init__(self, action, DF, upload_date):
         self.action = action
         self.DF = DF
         self.upload_date = upload_date
 
-    def Append_Errors(self, DF):
-        if not DF.empty:
-            if self.Errors.empty:
-                self.Errors = DF
-            else:
-                self.Errors.append(DF)
+    def CheckMapped(self, Gs_SrvType, Col):
+        DF_Results = pd.DataFrame()
+        data = self.DF.loc[self.DF['Gs_SrvType'] == Gs_SrvType]
 
-    def Validate(self):
-        def CheckMapped(SQL, ASQL, Gs_SrvType, sqltbl, Col):
+        if not data.empty:
+            SQL = SQLConnect('sql')
+            ASQL = SQLConnect('alch')
             SQL.connect()
             ASQL.connect()
 
-            DF_Results = pd.DataFrame()
+            ASQL.upload(data, 'mytbl')
 
-            data = self.DF.loc[self.DF['Gs_SrvType'] == Gs_SrvType]
+            DF_Results = SQL.query('''
+                select
+                    A.*
+                from mytbl As A
+                left join {0} As B
+                on
+                    A.Gs_SrvID = B.{1}
 
-            if not data.empty:
-                ASQL.upload(data, 'mytbl')
+                where
+                    B.{1} is null'''.format(Settings[Gs_SrvType],Col)
+            )
 
-                DF_Results = SQL.query('''
-                        select
-                            A.*
-                        from mytbl As A
-                        left join {0}.{1} As B
-                        on
-                            A.Gs_SrvID = B.{2}
+            DF_Results['Error_Columns'] = 'Gs_SrvType, Gs_SrvID'
+            DF_Results['Error_Message'] = 'Gs_SrvID in {0} is not found in {1}.{2}'.format(Gs_SrvType,Settings['Cus_Inv_Schema'],sqltbl)
 
-                        where
-                            B.{2} is null'''.format(Settings['Cus_Inv_Schema'],sqltbl,Col)
-                    )
-
-                DF_Results['Error_Columns'] = 'Gs_SrvType, Gs_SrvID'
-                DF_Results['Error_Message'] = 'Gs_SrvID in {0} is not found in {1}.{2}'.format(Gs_SrvType,Settings['Cus_Inv_Schema'],sqltbl)
-                ASQL.execute("drop table mytbl")
+            ASQL.execute("drop table mytbl")
 
             SQL.close()
             ASQL.close()
 
-            return DF_Results
+        Append_Errors(DF_Results)
 
-        SQL = SQLConnect('sql')
-        ASQL = SQLConnect('alch')
+        if data.empty:
+            self.Success = False
+        elif DF_Results.empty:
+            self.Success = True
+        elif not len(data.index) == len(DF_Results.index):
+            self.Success = True
+
+    def CheckDispute(self, Source_TBL):
+        data = self.DF.loc[self.DF['Source_TBL'] == Source_TBL]
+
+        if data:
+            ASQL = SQLConnect('alch')
+            SQL = SQLConnect('sql')
+            SQL.connect()
+            ASQL.connect()
+
+            ASQL.upload(data, 'mytbl')
+
+            if Source_TBL == 'BMI':
+                DF_Results = ASQL.query('''
+                    select
+                        A.*
+                    from mytbl As A
+                    left join {0} As B
+                    on
+                        A.Source_ID = B.BMI_ID
+
+                    where
+                        B.Invoice_Date > eomonth(dateadd(month, -1, A.Invoice_Date))
+                            and
+                        B.Amount > 0
+                            and
+                        B.BMI_ID is null'''.format(Settings['MRC'])
+
+                )
+            elif Source_TBL == 'PCI':
+                DF_Results = ASQL.query('''
+                    select
+                        A.*
+                    from mytbl As A
+                    left join {0} As B
+                    on
+                        A.Source_ID = B.{1}
+
+                    where
+                        B.{1} is null'''.format(Settings['PaperCost'])
+                )
+
+            SQL.close()
+            ASQL.close()
+
+    def Validate(self):
+        self.Success = False
 
         if self.action == 'Map':
-            self.Append_Errors(CheckMapped(SQL, ASQL, 'LL', 'ORDER_LOCAL_NEW', 'ORD_WTN'))
-            self.Append_Errors(CheckMapped(SQL, ASQL, 'BRD', 'ORDER_BROADBAND', 'ORD_BRD_ID'))
-            self.Append_Errors(CheckMapped(SQL, ASQL, 'DED', 'ORDER_DEDICATED', 'CUS_DED_ID'))
-            self.Append_Errors(CheckMapped(SQL, ASQL, 'LD', 'ORDER_1PLUS', 'ORD_WTN'))
-            self.Append_Errors(CheckMapped(SQL, ASQL, 'TF', 'ORDER_800', 'ORD_POTS_ANI_BIL'))
-            print(self.Errors)
+            self.CheckMapped('LL', 'ORD_WTN')
+            self.CheckMapped('BRD', 'ORD_BRD_ID')
+            self.CheckMapped('DED', 'CUS_DED_ID')
+            self.CheckMapped('LD', 'ORD_WTN')
+            self.CheckMapped('TF', 'ORD_POTS_ANI_BIL')
+        elif self.action == 'Dispute':
+            self.CheckDispute('STC', 'BMI')
+            self.CheckDispute('STC', 'PCI')
+            self.CheckDispute('Email', 'BMI')
+            self.CheckDispute('Email', 'PCI')
 
-        if not self.Errors.empty:
+        if self.Success:
             return True
