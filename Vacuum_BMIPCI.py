7211c1f56b228bde01fc61c3f8a46efb04f2b5e0
KLRussell <47677315+KLRussell@users.noreply.github.com>
2019-02-21 13:02:24 -0500

    Update Vacuum_BMIPCI.py

--- Vacuum_BMIPCI.py
+++ Vacuum_BMIPCI.py
@@ -6,7 +6,7 @@ import pandas as pd
 #self.DF.loc[self.DF['Gs_SrvType'] == 'LL',['Source_TBL','Source_ID','Gs_SrvID']]
 
 class BMIPCI:
-    Errors = None
+    Errors = []
 
     def __init__(self, action, DF, upload_date):
         self.action = action
@@ -15,30 +15,38 @@ class BMIPCI:
 
     def Validate(self):
         def CheckMapped(SQL, Gs_SrvType, sqltbl, Col):
+            SQL.connect()
+            ASQL.connect()
+
             data = self.DF.loc[self.DF['Gs_SrvType'] == Gs_SrvType]
-            SQL.createtable(data,'#My_TBL')
-            SQL.upload(data, '#My_TBL')
 
-            self.Errors.append(
-                [
-                SQL.query('''
-                    select
-                        A.*
-                    from #My_TBL As A
-                    left join {0}.{1} As B
-                    on
-                        A.Gs_SrvID = B.{2}
+            if not data.empty:
+                ASQL.upload(data, 'mytbl')
 
-                    where
-                        B.{2} is null'''.format(Settings['Cus_Inv_Schema'],sqltbl,Col)
-                ), 'Gs_SrvType, Gs_SrvID'
-                , 'Gs_SrvID in {0} is not found in {1}.{2}'.format(Gs_SrvType,Settings['Cus_Inv_Schema'],sqltbl)
-                ]
-            )
+                self.Errors.append(
+                    [
+                    SQL.query('''
+                        select
+                            A.*
+                        from mytbl As A
+                        left join {0}.{1} As B
+                        on
+                            A.Gs_SrvID = B.{2}
 
-            SQL.query('DROP TABLE #My_TBL')
+                        where
+                            B.{2} is null'''.format(Settings['Cus_Inv_Schema'],sqltbl,Col)
+                    ), 'Gs_SrvType, Gs_SrvID'
+                    , 'Gs_SrvID in {0} is not found in {1}.{2}'.format(Gs_SrvType,Settings['Cus_Inv_Schema'],sqltbl)
+                    ]
+                )
+
+                ASQL.execute("drop table mytbl")
+
+            SQL.close()
+            ASQL.close()
 
         SQL = SQLConnect('sql')
+        ASQL = SQLConnect('alch')
 
         if self.action == 'Map':
             CheckMapped(SQL, 'LL', 'ORDER_LOCAL_NEW', 'ORD_WTN')
@@ -46,10 +54,6 @@ class BMIPCI:
             CheckMapped(SQL, 'DED', 'ORDER_DEDICATED', 'CUS_DED_ID')
             CheckMapped(SQL, 'LD', 'ORDER_1PLUS', 'ORD_WTN')
             CheckMapped(SQL, 'TF', 'ORDER_800', 'ORD_POTS_ANI_BIL')
-        elif self.action == 'Dispute':
-            
-
-        SQL.close()
 
         if not self.Errors:
             return True
