06785368101d76bbd388ef241a53c041192ab76c
KLRussell <47677315+KLRussell@users.noreply.github.com>
2019-02-22 17:17:07 -0500

    Update Vacuum_BMIPCI.py

--- Vacuum_BMIPCI.py
+++ Vacuum_BMIPCI.py
@@ -29,6 +29,7 @@ class BMIPCI:
             DF_Results = SQL.query('''
                 select
                     A.*
+
                 from mytbl As A
                 left join {0} As B
                 on
@@ -39,7 +40,7 @@ class BMIPCI:
             )
 
             DF_Results['Error_Columns'] = 'Gs_SrvType, Gs_SrvID'
-            DF_Results['Error_Message'] = 'Gs_SrvID in {0} is not found in {1}.{2}'.format(Gs_SrvType,Settings['Cus_Inv_Schema'],sqltbl)
+            DF_Results['Error_Message'] = 'Gs_SrvID in {0} is not found in {1}'.format(Gs_SrvType,Settings[Gs_SrvType])
 
             ASQL.execute("drop table mytbl")
 
@@ -66,38 +67,239 @@ class BMIPCI:
 
             ASQL.upload(data, 'mytbl')
 
+            SQL.execute('''
+                update A
+                set A.Start_Date = dateadd(day, (-1 * B.Dispute_Limit) + 15, getdate())
+                from mytbl As A
+                inner join {0} As B
+                on
+                    A.BAN = B.BAN
+
+                where
+                    A.Start_Date is null
+            '''.format(Settings['Limitations']))
+
+            SQL.execute('''
+                update A
+                set A.Start_Date = eomonth(dateadd(month, -1, A.Start_Date))
+                from mytbl As A
+                where
+                    A.Start_Date is not null
+            ''')
+
             if Source_TBL == 'BMI':
-                DF_Results = ASQL.query('''
+                DF_Results = SQL.query('''
+                    with
+                        TMP
+                    As
+                    (
+                        select
+                            A.*
+
+                        from mytbl As A
+                        left join {0} As B
+                        on
+                            A.Source_ID = B.BMI_ID
+                                and
+                            B.Invoice_Date > A.Start_Date
+                                and
+                            B.Amount > 0
+
+                        where
+                            B.BDT_MRC_ID is null
+                    ),
+                        TMP2
+                    As
+                    (
+                        select
+                            A.BMI_ID,
+                            B.BDT_MRC_ID,
+                            B.Invoice_Date
+
+                        from {1} As A
+                        inner join {2} As B
+                        on
+                            A.Vendor = B.Vendor
+                                and
+                            A.BAN = B.BAN
+                                and
+                            A.WTN = B.BTN
+                                and
+                            A.Circuit_ID = B.Circuit_ID
+
+                        where
+                            B.Amount > 0
+                    )
+
                     select
                         A.*
-                    from mytbl As A
-                    left join {0} As B
+                    from TMP As A
+                    left join TMP2 As B
                     on
                         A.Source_ID = B.BMI_ID
+                            and
+                        A.Start_Date < B.Invoice_Date
 
                     where
-                        B.Invoice_Date > eomonth(dateadd(month, -1, A.Invoice_Date))
-                            and
-                        B.Amount > 0
-                            and
-                        B.BMI_ID is null'''.format(Settings['MRC'])
-
+                        B.BDT_OCC_ID is null'''.format(Settings['MRC'],Settings['BMI'],Settings['OCC'])
                 )
+
+                DF_Results['Error_Columns'] = 'Source_TBL, Source_ID'
+                DF_Results['Error_Message'] = 'Valid cost for Source_ID of {0} is not found in MRC or OCC tables'.format(Source_TBL)
+
             elif Source_TBL == 'PCI':
-                DF_Results = ASQL.query('''
+                DF_Results = SQL.query('''
                     select
                         A.*
                     from mytbl As A
                     left join {0} As B
                     on
-                        A.Source_ID = B.{1}
+                        A.Source_ID = B.PCI_ID
+                            and
+                        B.Bill_Date > A.Start_Date
+                            and
+                        (B.MRC > 0 or B.FRAC > 0 or B.NRC > 0)
 
                     where
-                        B.{1} is null'''.format(Settings['PaperCost'])
+                        B.Seed is null'''.format(Settings['PaperCost'])
                 )
+
+                DF_Results['Error_Columns'] = 'Source_TBL, Source_ID'
+                DF_Results['Error_Message'] = 'Valid cost for Source_ID of {0} is not found in Papercost'.format(Source_TBL)
+
+            ASQL.execute("drop table mytbl")
 
             SQL.close()
             ASQL.close()
+
+            Append_Errors(DF_Results)
+
+        if data.empty:
+            self.Success = False
+        elif DF_Results.empty:
+            self.Success = True
+        elif not len(data.index) == len(DF_Results.index):
+            self.Success = True
+
+    def CheckProv(self):
+        ASQL = SQLConnect('alch')
+        SQL = SQLConnect('sql')
+        SQL.connect()
+        ASQL.connect()
+
+        ASQL.upload(self.DF, 'mytbl')
+
+        ASQL.execute("drop table mytbl")
+
+        DF_Results = SQL.query('''
+            select
+                A.*,
+                iif(B.MACNUM is null,'Macnum','Source_TBL, Source_ID') As Error_Columns,
+                iif(B.MACNUM is null,'This Macnum does not exist in CS','This Source_TBL and Source_ID is already pending in Send to Prov table') As Error_Message
+
+            from mytbl As A
+            left join {0} As B
+            on
+                A.Macnum = B.Macnum
+            left join {1} As C
+            on
+                A.Source_TBL = C.Source_TBL
+                    and
+                A.Source_ID = C.Source_ID
+
+            where
+                B.MACNUM is null
+                    or
+                (
+                    C.Root_Cause is null
+                        and
+                    C.Prov_Note is null
+                        and
+                    C.New_Root_Cause is null
+                        and
+                    C.Audit_ID is not null
+                )'''.format(Settings['Cust_File'], Settings['Send_To_Prov'])
+        )
+
+        SQL.close()
+        ASQL.close()
+
+        Append_Errors(DF_Results)
+
+        if DF_Results.empty:
+            self.Success = True
+        elif not len(self.DF.index) == len(DF_Results.index):
+            self.Success = True
+
+    def CheckLV(self):
+        ASQL = SQLConnect('alch')
+        SQL = SQLConnect('sql')
+        SQL.connect()
+        ASQL.connect()
+
+        ASQL.upload(self.DF, 'mytbl')
+
+        DF_Results = SQL.query('''
+            select
+                A.*
+            from mytbl As A
+            left join {0} As B
+            on
+                A.Macnum = B.Macnum
+
+            where
+                B.MACNUM is null'''.format(Settings['Cust_File'])
+        )
+
+        DF_Results['Error_Columns'] = 'Macnum'
+        DF_Results['Error_Message'] = 'This Macnum does not exist in CS'
+
+        ASQL.execute("drop table mytbl")
+
+        SQL.close()
+        ASQL.close()
+
+        Append_Errors(DF_Results)
+
+        if DF_Results.empty:
+            self.Success = True
+        elif not len(self.DF.index) == len(DF_Results.index):
+            self.Success = True
+
+    def CheckDN(self):
+        ASQL = SQLConnect('alch')
+        SQL = SQLConnect('sql')
+        SQL.connect()
+        ASQL.connect()
+
+        ASQL.upload(self.DF, 'mytbl')
+
+        DF_Results = SQL.query('''
+            select
+                A.*
+            from mytbl As A
+            left join {0} As B
+            on
+                A.Macnum = B.Macnum
+
+            where
+                B.MACNUM is null'''.format(Settings['Cust_File'])
+        )
+
+        DF_Results['Error_Columns'] = 'Macnum'
+        DF_Results['Error_Message'] = 'This Macnum does not exist in CS'
+
+        ASQL.execute("drop table mytbl")
+
+        SQL.close()
+        ASQL.close()
+
+        Append_Errors(DF_Results)
+
+        if DF_Results.empty:
+            self.Success = True
+        elif not len(self.DF.index) == len(DF_Results.index):
+            self.Success = True
 
     def Validate(self):
         self.Success = False
@@ -109,10 +311,14 @@ class BMIPCI:
             self.CheckMapped('LD', 'ORD_WTN')
             self.CheckMapped('TF', 'ORD_POTS_ANI_BIL')
         elif self.action == 'Dispute':
-            self.CheckDispute('STC', 'BMI')
-            self.CheckDispute('STC', 'PCI')
-            self.CheckDispute('Email', 'BMI')
-            self.CheckDispute('Email', 'PCI')
+            self.CheckDispute('BMI')
+            self.CheckDispute('PCI')
+        elif self.action == 'Sent to Prov':
+            self.CheckProv()
+        elif self.action == 'Sent to LV':
+            self.CheckLV()
+        elif self.action == 'Dispute Note':
+            self.CheckDN()
 
         if self.Success:
             return True
