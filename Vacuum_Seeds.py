795e30720c624fa31bd3bb0d310218e030a6b935
KLRussell <47677315+KLRussell@users.noreply.github.com>
2019-03-05 18:24:26 -0500

    Update Vacuum_Seeds.py

--- Vacuum_Seeds.py
+++ Vacuum_Seeds.py
@@ -3,10 +3,24 @@ from Vacuum_Global import SQLConnect
 
 
 class Seeds:
-    Errors = None
 
-    def __init__(self, cost_type, df, folder_name, upload_date):
-        self.Cost_Type = cost_type
+    def __init__(self, cost_type, df, folder_name):
+        self.cost_type = cost_type
         self.df = df
         self.folder_name = folder_name
-        self.upload_date = upload_date
+
+    def mrc(self, dispute_type):
+        asql = SQLConnect('alch')
+        asql.connect()
+
+        asql.upload(self.df, 'mytbl')
+
+        asql.execute('drop table mytbl')
+
+        asql.close()
+
+    def process(self):
+        if self.cost_type == 'MRC':
+            self.mrc('GRT')
+            self.mrc('Email')
+
