6a1550fdf36d6a4bd07c3eeffe5660405cd0b494
KLRussell <47677315+KLRussell@users.noreply.github.com>
2019-03-05 12:30:59 -0500

    Update Vacuum_NonSeeds.py

--- Vacuum_NonSeeds.py
+++ Vacuum_NonSeeds.py
@@ -1,14 +1,11 @@
-from Vacuum_Global import Settings
+from Vacuum_Global import settings
 from Vacuum_Global import SQLConnect
 
-import pandas as pd
-
-#self.DF.loc[self.DF['Gs_SrvType'] == 'LL',['Source_TBL','Source_ID','Gs_SrvID']]
 
 class NonSeeds:
     Errors = None
 
-    def __init__(self, DF, folder_name, upload_date):
-        self.DF = DF
+    def __init__(self, df, folder_name, upload_date):
+        self.df = df
         self.folder_name = folder_name
         self.upload_date = upload_date
