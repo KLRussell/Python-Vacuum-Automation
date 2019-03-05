eac5e17130f49ac004f89416a7f7d30ecd932e7d
KLRussell <47677315+KLRussell@users.noreply.github.com>
2019-03-05 12:30:44 -0500

    Update Vacuum_Seeds.py

--- Vacuum_Seeds.py
+++ Vacuum_Seeds.py
@@ -1,15 +1,12 @@
-from Vacuum_Global import Settings
+from Vacuum_Global import settings
 from Vacuum_Global import SQLConnect
 
-import pandas as pd
-
-#self.DF.loc[self.DF['Gs_SrvType'] == 'LL',['Source_TBL','Source_ID','Gs_SrvID']]
 
 class Seeds:
     Errors = None
 
-    def __init__(self, Cost_Type, DF, folder_name, upload_date):
-        self.Cost_Type = Cost_Type
-        self.DF = DF
+    def __init__(self, cost_type, df, folder_name, upload_date):
+        self.Cost_Type = cost_type
+        self.df = df
         self.folder_name = folder_name
         self.upload_date = upload_date
