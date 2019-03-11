07f3fa4bc9933695a4d9b4a4741f528820f83a67
KLRussell <47677315+KLRussell@users.noreply.github.com>
2019-03-11 17:55:32 -0400

    Migrated core elements for escates, dispute notes, and paid from Vacuum_BMIPCI script. Functions do need to be fixed and incorported in script

--- Vacuum_DisputeActions.py
+++ Vacuum_DisputeActions.py
@@ -4,6 +4,7 @@ from Vacuum_Global import validatecol
 from Vacuum_Global import processresults
 from Vacuum_Global import writelog
 from Vacuum_Global import defaultheader
+from Vacuum_Global import getbatch
 
 
 class DisputeActions:
@@ -13,10 +14,14 @@ class DisputeActions:
         self.folder_name = folder_name
         self.asql = asql
 
-        self.df = defaultheader(self.df, '''dispute_id, action, amount, credit_invoice_date, action_norm_reason
-            , action_reason, assign_rep, note_tag, attachment, ilec_confirmation, error_columns, error_message''')
+        if not self.df.empty:
+            self.df = defaultheader(self.df, '''dispute_id, action, amount, credit_invoice_date, action_norm_reason
+                , action_reason, assign_rep, note_tag, attachment, ilec_confirmation, error_columns, error_message''')
 
     def escalate(self):
+        if self.asql.query("select object_id('DH')").iloc[0, 0]:
+            self.asql.execute("DROP TABLE DH")
+
         self.asql.execute("CREATE TABLE DH (DH_ID int, DSB_ID int)")
 
         self.asql.execute('''
@@ -26,60 +31,260 @@ class DisputeActions:
                 Dispute_Category,
                 Display_Status,
                 Date_Submitted,
+                ILEC_Confirmation,
+                ILEC_Comments,
                 Escalate,
                 Escalate_DT,
                 Escalate_Amount,
                 Dispute_Reason,
                 STC_Index,
                 GRT_Update_Rep,
+                Resolution_Date,
                 Date_Updated,
                 Source_File
             )
+            OUTPUT
+                INSERTED.DH_ID,
+                INSERTED.DSB_ID
+                
+            INTO
+                DH
 
+            SELECT
+                A.DSB_ID,
+                C.Dispute_Category,
+                'GRT Escalate',
+                C.Date_Submitted,
+                C.ILEC_Confirmation,
+                C.LEC_Comments,
+                'Y',
+                cast(getdate() as date),
+                C.Dispute_Amount,
+                A.Action_Reason,
+                C.STC_Index,
+                B.Full_Name,
+                C.Resolution_Date,
+                getdate(),
+                'GRT Status: {3}'
+
+            FROM mydisputes As A
+            INNER JOIN {1} As B
+            ON
+                A.Comp_Serial = B.Comp_Serial
+            inner join {2} As C
+            on
+                A.DSB_ID = C.DSB_ID
+        '''.format(settings['Dispute_History'], settings['CAT_Emp'], settings['Dispute_Current'], getbatch()))
+
+        self.asql.execute('''
+            UPDATE B
+            SET
+                B.Display_Status = 'GRT Escalate',
+                B.Escalate_Date = cast(getdate() as date),
+                B.#_Of_Escalations = case when B.#_Of_Escalations is null then 1 else B.#_Of_Escalations + 1 end,
+                B.Dispute_Reason = A.Action_Reason,
+                B.Last_GRT_Action = 'GRT Escalate',
+                B.Last_GRT_Action_Rep = C.Full_Name,
+                B.Date_Updated = getdate(),
+                B.Source_File = 'GRT Status: {2}',
+                B.DH_ID = DH.DH_ID
+
+            FROM mydisputes As A
+            INNER JOIN {0} As B
+            ON
+                A.DSB_ID = B.DSB_ID
+            INNER JOIN {1} As C
+            ON
+                A.Comp_Serial = C.Comp_Serial
+            INNER JOIN DH
+            ON
+                A.DSB_ID = DH.DSB_ID
+        '''.format(settings['Dispute_Current'], settings['CAT_Emp'], getbatch()))
+
+        self.asql.execute('DROP TABLE DH')
+
+    def close(self):
+        if self.asql.query("select object_id('DH')").iloc[0, 0]:
+            self.asql.execute("DROP TABLE DH")
+
+        self.asql.execute("CREATE TABLE DH (DH_ID int, DSB_ID int)")
+
+        self.asql.execute('''
+            INSERT INTO {0}
+            (
+                DSB_ID,
+                Dispute_Category,
+                Display_Status,
+                Date_Submitted,
+                ILEC_Confirmation,
+                ILEC_Comments,
+                Escalate,
+                Escalate_DT,
+                Escalate_Amount,
+                Dispute_Reason,
+                STC_Index,
+                Norm_Close_Reason,
+                Close_Reason,
+                GRT_Update_Rep,
+                Resolution_Date,
+                Date_Updated,
+                Source_File
+            )
             OUTPUT
                 INSERTED.DH_ID,
                 INSERTED.DSB_ID
 
-            INTO DH
+            INTO
+                DH
 
             SELECT
                 A.DSB_ID,
-                A.Dispute_Category,
-                'GRT Escalate',
-                cast(getdate() as date),
-                1,
-                cast(getdate() as date),
-                A.Dispute_Amount,
+                D.Dispute_Category,
+                'Denied - Closed',
+                D.Date_Submitted,
+                D.ILEC_Confirmation,
+                D.ILEC_Comments,
+                D.Escalate,
+                D.Escalate_DT,
+                D.Escalate_Amount
+                D.Dispute_Reason,
+                D.STC_Index,
+                A.Action_Norm_Reason,
                 A.Action_Reason,
-                A.STC_Index,
                 B.Full_Name,
+                B.Resolution_Date,
                 getdate(),
-                'GRT Status: {2}'
+                'GRT Status: {3}'
 
-            FROM mytbl2 As A
+            FROM mydisputes As A
             INNER JOIN {1} As B
             ON
                 A.Comp_Serial = B.Comp_Serial
-        '''.format(settings['Dispute_History'], settings['CAT_Emp'], getbatch()))
+            inner join {2} As C
+            on
+                A.DSB_ID = C.DSB_ID
+            inner join {0} As D
+            on
+                A.DH_ID = C.DH_ID
+        '''.format(settings['Dispute_History'], settings['CAT_Emp'], settings['Dispute_Current'], getbatch()))
 
         self.asql.execute('''
-            UPDATE A
+            UPDATE B
             SET
-                A.DH_ID = B.DH_ID
+                B.Display_Status = 'Denied - Closed',
+                B.Norm_Close_Reason = A.Action_Norm_Reason,
+                B.Close_Reason = A.Action_Reason,
+                B.Last_GRT_Action = 'GRT Escalate',
+                B.Last_GRT_Action_Rep = C.Full_Name,
+                B.Date_Updated = getdate(),
+                B.Source_File = 'GRT Status: {2}',
+                B.DH_ID = DH.DH_ID
 
-            FROM {0} As A
-            INNER JOIN DH As B
+            FROM mydisputes As A
+            INNER JOIN {0} As B
             ON
                 A.DSB_ID = B.DSB_ID
-        '''.format(settings['Dispute_Fact']))
-
-    def close(self):
-        # migrate core elements from Vacuum_Global close function
-        self.asql.execute('''
-
-        ''')
+            INNER JOIN {1} As C
+            ON
+                A.Comp_Serial = C.Comp_Serial
+            INNER JOIN DH
+            ON
+                A.DSB_ID = DH.DSB_ID
+        '''.format(settings['Dispute_Current'], settings['CAT_Emp'], getbatch()))
 
     def paid(self):
+        if self.asql.query("select object_id('DH')").iloc[0, 0]:
+            self.asql.execute("DROP TABLE DH")
+
+        self.asql.execute("CREATE TABLE DH (DH_ID int, DSB_ID int)")
+
+        self.asql.execute('''
+            INSERT INTO {0}
+            (
+                DSB_ID,
+                Dispute_Category,
+                Display_Status,
+                Date_Submitted,
+                ILEC_Confirmation,
+                ILEC_Comments,
+                Credit_Approved,
+                Denied,
+                Credit_Received_Amount,
+                Credit_Received_Invoice_Date,
+                Escalate,
+                Escalate_DT,
+                Escalate_Amount,
+                Dispute_Reason,
+                STC_Index,
+                GRT_Update_Rep,
+                Resolution_Date,
+                Date_Updated,
+                Source_File
+            )
+            OUTPUT
+                INSERTED.DH_ID,
+                INSERTED.DSB_ID
+
+            INTO
+                DH
+
+            SELECT
+                A.DSB_ID,
+                D.Dispute_Category,
+                'Paid',
+                D.Date_Submitted,
+                D.ILEC_Confirmation,
+                D.ILEC_Comments,
+                A.
+                D.Escalate,
+                D.Escalate_DT,
+                D.Escalate_Amount
+                D.Dispute_Reason,
+                D.STC_Index,
+                A.Action_Norm_Reason,
+                A.Action_Reason,
+                B.Full_Name,
+                getdate(),
+                'GRT Status: {3}'
+
+            FROM mydisputes As A
+            INNER JOIN {1} As B
+            ON
+                A.Comp_Serial = B.Comp_Serial
+            inner join {2} As C
+            on
+                A.DSB_ID = C.DSB_ID
+            inner join {0} As D
+            on
+                A.DH_ID = C.DH_ID
+        '''.format(settings['Dispute_History'], settings['CAT_Emp'], settings['Dispute_Current'], getbatch()))
+
+        self.asql.execute('''
+            UPDATE B
+            SET
+                B.Display_Status = 'Denied - Closed',
+                B.Norm_Close_Reason = A.Action_Norm_Reason,
+                B.Close_Reason = A.Action_Reason,
+                B.Last_GRT_Action = 'GRT Escalate',
+                B.Last_GRT_Action_Rep = C.Full_Name,
+                B.Date_Updated = getdate(),
+                B.Source_File = 'GRT Status: {2}',
+                B.DH_ID = DH.DH_ID
+
+            FROM mydisputes As A
+            INNER JOIN {0} As B
+            ON
+                A.DSB_ID = B.DSB_ID
+            INNER JOIN {1} As C
+            ON
+                A.Comp_Serial = C.Comp_Serial
+            INNER JOIN DH
+            ON
+                A.DSB_ID = DH.DSB_ID
+        '''.format(settings['Dispute_Current'], settings['CAT_Emp'], getbatch()))
+
+
+
         self.asql.execute("CREATE TABLE DH (DH_ID int, DSB_ID int)")
 
         self.asql.execute('''
@@ -179,8 +384,6 @@ class DisputeActions:
 
             from mydisputes
         '''.format(settings['Dispute_Notes']))
-
-
 
     def process(self):
         if not self.asql:
